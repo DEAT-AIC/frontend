f2edf86d4347595f01bbe8b94088f004c0d11b46
DamarKusumo <muhammaddamarkusumo@gmail.com>
2023-09-02 23:05:18 +0700

    Feat: Auth Pages

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  tittleCard: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: "center",
+    paddingBottom:20
+  },
+  subtittleCard: {
+    fontSize: 12,
+    fontWeight: 'semibold'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    marginTop: 5,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    opacity: 0.75,
+    borderRadius: 8
+  },
+});
