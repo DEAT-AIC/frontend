9966968fb780297dcf89d350c23fcb3d2cc3fc9e
Muhammad Damar Kusumo <125840036+DamarKusumo@users.noreply.github.com>
2023-09-03 10:09:28 +0700

    Feat: Auth Save User Data (#4)
    * Add Routing
    
    * Feat: Auth Pages
    
    * Feat: Login Save User
    
    * Feat: Login Save User

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import Navigator from './src/routes/routes';
+import React, { useState, useEffect } from 'react';
+import { AuthNavigator, HomeNavigator } from './src/routes/routes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 
+  const [isLogin, setIsLogin] = useState(false);
+
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('user');
+      if (value !== null) {
+        console.log(JSON.parse(value));
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+      console.log(value);
+    } catch (error) {
+      // Error retrieving data
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    _retrieveData();
+  }, [])
+
   return (
-    <Navigator />
+    <NavigationContainer>
+      {isLogin ? <HomeNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
   )
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,5 @@
 
-import React , { useState , useEffect , useContext } from 'react';
-import { StyleSheet, Text, Image ,View} from 'react-native';
-// import { createStackNavigator } from 'react-navigation-stack';
-// import { createAppContainer } from 'react-navigation';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect, useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { auth , onAuthStateChanged   } from "../utils/firebase";
 // import { useDispatch, useSelector } from 'react-redux';
@@ -19,17 +15,26 @@ import RegisterProfile from '../screens/registerProfile';
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+const AuthNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Landing" component={Landing} options={{ title: 'Welcome', headerLeft:null }}/>
-        <Stack.Screen name="Login" component={Login} options={{}}/>
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="RegisterProfile" component={RegisterProfile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Landing" component={Landing} options={{ title: 'Welcome', headerLeft: null }} />
+      <Stack.Screen name="Login" component={Login} options={{}} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="RegisterProfile" component={RegisterProfile} />
+    </Stack.Navigator>
   );
 }
 
-export default App;
+const HomeNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Landing" component={Landing} options={{ title: 'Welcome', headerLeft: null }} />
+      <Stack.Screen name="Login" component={Login} options={{}} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="RegisterProfile" component={RegisterProfile} />
+    </Stack.Navigator>
+  );
+}
+
+export { AuthNavigator, HomeNavigator };
